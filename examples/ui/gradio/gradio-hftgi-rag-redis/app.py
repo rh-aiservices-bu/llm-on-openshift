927bc5cb2d968a17c0775553b785e0b5c34fe432
Twinkll Sisodia <tsisodia@redhat.com>
2023-11-21 16:08:30 -0500

    Code refinement

--- examples/ui/gradio/gradio-hftgi-rag-redis/app.py
+++ examples/ui/gradio/gradio-hftgi-rag-redis/app.py
@@ -42,8 +42,6 @@ start_http_server(8000)
 FEEDBACK_COUNTER = Counter("feedback_stars", "Number of feedbacks by stars", ["stars", "model_id"])
 MODEL_USAGE_COUNTER = Counter('model_usage', 'Number of times a model was used', ['model_id'])
 REQUEST_TIME = Gauge('request_duration_seconds', 'Time spent processing a request', ['model_id'])
-SATISFACTION = Gauge('satisfaction_rating', 'User satisfaction rating', ['rating'])
-TIMEOUTS = Counter('timeouts_total', 'Total number of request timeouts', ['model_id'])
 
 model_id = ""
 
@@ -69,22 +67,13 @@ def remove_source_duplicates(input_list):
             unique_list.append(item.metadata['source'])
     return unique_list
 
-def get_model_info():
-    response = requests.get(INFERENCE_SERVER_URL + "/info")
-    json_response = response.json()
-    print(json_response)
-    model_id = json_response['model_id']  # Extract the model_id
-    print("Model ID:", model_id)  # Print the model_id
-    return model_id  # Return the model_id instead of the whole JSON
-
 
 def stream(input_text) -> Generator:
 
     global model_id
-    # Create a Queue
+    # Create queue
     job_done = object()
 
-    # Create a function to call - this will run in a thread
     def task():
         resp = qa_chain({"query": input_text})
         sources = remove_source_duplicates(resp['source_documents'])  
@@ -92,17 +81,17 @@ def stream(input_text) -> Generator:
         start_time = time.perf_counter() # start and end time to get the precise timing of the request
         
         try:
-            # response = client.generate(input, max_new_tokens=MAX_NEW_TOKENS)
-            model_id = get_model_info()
+            response = requests.get(INFERENCE_SERVER_URL + "/info")
+            json_response = response.json()
+            print(json_response)
+            model_id = json_response['model_id']
             end_time = time.perf_counter()
             # Record successful request time
             REQUEST_TIME.labels(model_id=model_id).set(end_time - start_time)
         except TimeoutError:  # or whatever exception your client throws on timeout
             end_time = time.perf_counter()
-            TIMEOUTS.info({'model_id': model_id, 'timeout_duration': str(end_time - start_time), 'input_text': input})
 
         q.put({"model_id": model_id})
-        # q.put({"generated_text": resp.generated_text})
         print("MODEL ID IS:",model_id)
         print("Question:",input)
         if len(sources) != 0:
@@ -126,9 +115,7 @@ def stream(input_text) -> Generator:
                 break   
             if isinstance(next_token, dict) and 'model_id' in next_token:
                 model_id = next_token['model_id']
-                MODEL_USAGE_COUNTER.labels(model_id=model_id).inc()
-            # if isinstance(next_token, dict) and 'generated_text' in next_token:
-            #     generated_text = next_token['generated_text']    
+                MODEL_USAGE_COUNTER.labels(model_id=model_id).inc()    
             elif isinstance(next_token, str):
                 content += next_token     
                 yield next_token, content, model_id
@@ -196,33 +183,33 @@ def ask_llm(message, history):
 
 
 # Gradio implementation
-with gr.Blocks(title="HatBot", css="footer {visibility: hidden}") as demo:    
+with gr.Blocks(title="HatBot") as demo:    
 
-    input_box = gr.Textbox(label="Your Question")
-    output_answer = gr.Textbox(label="Answer", readonly=True)
+    with gr.Column(class_name="column-class"):
+        project_box = gr.Textbox(label="Your Project")
+        customer_box = gr.Textbox(label="Customer")
+        input_box = gr.Textbox(label="Your Question")
+        submit_button = gr.Button("Submit")
+        output_answer = gr.Textbox(label="Answer", readonly=True)
+
     model_id_box = gr.Textbox(visible=False)  # will hold the model_id
 
-    gr.Interface(
+    submit_button.click(
         fn=ask_llm,
         inputs=[input_box],
-        outputs=[output_answer],
-        clear_btn=None,
-        retry_btn=None,
-        undo_btn=None,
-        stop_btn=None,
-        description=APP_TITLE
-        )    
+        outputs=[output_answer]
+    )
     
     radio = gr.Radio(["1", "2", "3", "4", "5"], label="Star Rating")
     output = gr.Textbox(label="Output Box")
 
+    download_button = gr.Button("Download")
+    
     @radio.input(inputs=radio, outputs=output)
     def get_feedback(star):
         print("Rating: " + star)
         # Increment the counter based on the star rating received
         FEEDBACK_COUNTER.labels(stars=str(star), model_id=model_id).inc()
-        SATISFACTION.labels(rating=star).set(1)
-
         return f"Received {star} star feedback. Thank you!"
 
 
