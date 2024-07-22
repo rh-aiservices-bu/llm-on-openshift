8439d132352ed5c71b64c760190732ed5aaae874
Dharit Shah <67937327+dhshah13@users.noreply.github.com>
2024-07-22 16:47:52 -0400

    added additional argument
    parser.add_argument('--trust_remote_code', type=bool, required=False, default=False, help='Trust remote code (default: False)')

--- llm-servers/sbert/gpu/app.py
+++ llm-servers/sbert/gpu/app.py
@@ -29,10 +29,11 @@ async def process_strings(input_array: StringArray):
         raise HTTPException(status_code=500, detail=str(e))
 
 parser = argparse.ArgumentParser(description='Load a Sentence Transformer model.')
-parser.add_argument('--model_path', type=str, required=False, default='/mnt/models', help='Path to the Sentence Transformer model', trust_remote_code=False)
+parser.add_argument('--model_path', type=str, required=False, default='/mnt/models', help='Path to the Sentence Transformer model')
+parser.add_argument('--trust_remote_code', type=bool, required=False, default=False, help='Trust remote code (default: False)')
 args = parser.parse_args()
 
-model = SentenceTransformer(args.model_path, args.trust_remote_code)
+model = SentenceTransformer(args.model_path, trust_remote_code=args.trust_remote_code)
 
 # Launch the FastAPI server
 if __name__ == "__main__":
